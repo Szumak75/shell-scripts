32a24abcc8b5d9c2a29c28f2292674ef11ed1f1d
Jacek Kotlarski <szumak@virthost.pl>
2025-01-31 10:58:10 +0100

    "Removed redundant 'sudo' from apt autoremove and autoclean commands."

--- apt-upgrade.sh
+++ apt-upgrade.sh
@@ -2,4 +2,4 @@
 
 # (Debian|Ubuntu) APT upgrade sequence
 
-sudo apt update && sudo apt upgrade && sudo apt autoremove --purge -y sudo && sudo apt autoclean -y
+sudo apt update && sudo apt upgrade && sudo apt autoremove --purge -y && sudo apt autoclean -y
