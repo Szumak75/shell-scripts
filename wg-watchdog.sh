1f81cb0d8f3dcfbab1ad55ba72da42e99a8370e1
Jacek 'Szumak' Kotlarski <szumak@virthost.pl>
2025-09-19 13:32:06 +0200

    `Refactor ping check in wg-watchdog.sh: increased ping attempts from 1 to 3.`

--- wg-watchdog.sh
+++ wg-watchdog.sh
@@ -5,9 +5,13 @@
 #  Created: 29.08.2025, 13:33:06
 #  
 #  Purpose: Watchdog script for WireGuard VPN on TASK
+#
+# [crontab]
+# m h  dom mon dow   command
+# * * * * * /opt/bin/wg-watchdog.sh > /dev/null 2>@1
 
 # Check if a specific server can be pinged
-if ping -c 1 10.240.1.1 > /dev/null 2>&1; then
+if ping -c 3 10.240.1.1 > /dev/null 2>&1; then
     logger -t "wg-watchdog" "WireGuard is working."
 else
     logger -t "wg-watchdog" "WireGuard is down. Attempting to restart."
